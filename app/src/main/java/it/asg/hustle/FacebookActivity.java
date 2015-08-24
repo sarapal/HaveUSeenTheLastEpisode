b068eeb033e3e1a560a371ff5b227753bde65ea6
Andrea <hybridcore23@gmail.com>
2015-08-24 17:26:37 +0200

    added facebook activity

--- app/src/main/java/it/asg/hustle/FacebookActivity.java
+++ app/src/main/java/it/asg/hustle/FacebookActivity.java
@@ -1,41 +1,22 @@
 package it.asg.hustle;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    Button facebookButton;
+public class FacebookActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-        /*apertura activity facebook*/
-        final Intent intentactivityfacebook = new Intent(this, FacebookActivity.class);
-        facebookButton = (Button) findViewById(R.id.activitybuttonFacebook);
-        facebookButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(intentactivityfacebook);
-            }
-        });
-
-
-
+        setContentView(R.layout.activity_facebook);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_facebook, menu);
         return true;
     }
 
