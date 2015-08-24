5b4fdf100579aeb58466ff30ff95ec58fc21d5b5
Andrea <hybridcore23@gmail.com>
2015-08-24 18:56:04 +0200

    added facebook activity

--- app/src/main/java/it/asg/hustle/FacebookActivity.java
+++ app/src/main/java/it/asg/hustle/FacebookActivity.java
@@ -1,16 +1,36 @@
 package it.asg.hustle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.FacebookSdk;
 
 public class FacebookActivity extends AppCompatActivity {
+
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facebook);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        // Initialize the SDK before executing any other operations,
+        // especially, if you're using Facebook UI elements.
+        loginButton = (Button) findViewById(R.id.login_button);
+        final Intent intent = new Intent(this, com.facebook.FacebookActivity.class);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
