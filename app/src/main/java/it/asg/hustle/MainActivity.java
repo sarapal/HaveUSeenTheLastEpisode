86e747f9d3ba98db41a6cc5ac368efc613afaf71
Sara <star.alnair@gmail.com>
2015-08-24 20:16:17 +0200

    layout

--- app/src/main/java/it/asg/hustle/MainActivity.java
+++ app/src/main/java/it/asg/hustle/MainActivity.java
@@ -2,21 +2,63 @@ package it.asg.hustle;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-    Button facebookButton;
+    private Button facebookButton;
+    private Toolbar toolbar;
+    private DrawerLayout myDrawerLayout;    //imposta NavigationDrawer
+    private FloatingActionButton fab;
+    private NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // imposto ActionBar sulla Toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_action);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
+        //prendo DrawerLayout
+        myDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        //click elementi su NavigationDrawer
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                myDrawerLayout.closeDrawers();
+                Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+        //click del FAB
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("asg","FAB was pressed");
+            }
+        });
 
         /*apertura activity facebook*/
         final Intent intentactivityfacebook = new Intent(this, FacebookActivity.class);
@@ -46,9 +88,12 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case android.R.id.home:
+                myDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.action_settings:
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
