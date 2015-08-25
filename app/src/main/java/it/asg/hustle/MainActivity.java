c788f0c92e4478f098a96fd35285b540d99963d5
Sara <star.alnair@gmail.com>
2015-08-25 16:43:37 +0200

    fragment + adapter (for tabs)

--- app/src/main/java/it/asg/hustle/MainActivity.java
+++ app/src/main/java/it/asg/hustle/MainActivity.java
@@ -4,17 +4,26 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -56,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("asg","FAB was pressed");
+                Log.d("asg", "FAB was pressed");
             }
         });
 
@@ -70,7 +79,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        TvShowAdapter adapter = new TvShowAdapter(getSupportFragmentManager());
+        ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
 
@@ -98,4 +111,57 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    // sottoclasse per gestire i fragment della pagina inziale
+    public static class TvShowFragment extends Fragment {
+        private static final String TAB_POSITION = "tab_position";
+
+        public TvShowFragment() {
+
+        }
+
+        public static TvShowFragment newInstance(int tabPosition) {
+            TvShowFragment fragment = new TvShowFragment();
+            Bundle args = new Bundle();
+            args.putInt(TAB_POSITION, tabPosition);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            Bundle args = getArguments();
+            int tabPosition = args.getInt(TAB_POSITION);
+            TextView tab = new TextView(getActivity());
+            tab.setGravity(Gravity.CENTER);
+            tab.setText("My Series " + tabPosition);
+            return tab;
+        }
+    }
+
+    //sottoclasse per l'adapter per i fragment (delle varie tab)
+    static class TvShowAdapter extends FragmentStatePagerAdapter {
+
+        public TvShowAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return TvShowFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Tab " + position;
+        }
+    }
+
 }
