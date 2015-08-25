6a9ea81bb0a4ef841860404bdf46dc88368bb0bc
Andrea <hybridcore23@gmail.com>
2015-08-25 17:01:40 +0200

    fix facebook activity

--- app/src/main/java/it/asg/hustle/FacebookActivity.java
+++ app/src/main/java/it/asg/hustle/FacebookActivity.java
@@ -63,6 +63,7 @@ public class FacebookActivity extends AppCompatActivity {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
