e52a93f762ceaa282dd94b1450b085e72f12e38f
Andrea <hybridcore23@gmail.com>
2015-09-17 11:05:46 +0200

    friends card now works!

--- app/src/main/java/it/asg/hustle/ShowActivity.java
+++ app/src/main/java/it/asg/hustle/ShowActivity.java
@@ -79,6 +79,7 @@ public class ShowActivity extends AppCompatActivity {
             show.fillSeasonsList(seasonsJSON);
         }
 
+
         //caso in cui l'activity viene generata dalla ricerca
         if(savedInstanceState == null){
             Bundle b = getIntent().getExtras();
